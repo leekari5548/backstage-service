9dcd176df2cfeeb9464dd8745165d91bfb0148b6
leetao <673416736@qq.com>
2020-07-30 07:50:11 +0800

    commit

--- src/main/java/com/leekari/wechat/WeChatApplication.java
+++ src/main/java/com/leekari/wechat/WeChatApplication.java
@@ -2,13 +2,28 @@ package com.leekari.wechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class WeChatApplication {
+public class WeChatApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WeChatApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WeChatApplication.class);
+    }
+}
+
+@RestController
+class IndexController{
+    @RequestMapping("/")
+    public String index(){
+        return "this is a service";
+    }
 }
