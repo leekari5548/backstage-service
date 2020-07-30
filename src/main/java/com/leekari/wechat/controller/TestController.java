3f10fdddfbbd6b726f6b1308a00007ac95d8037d
leetao <673416736@qq.com>
2020-07-30 17:39:45 +0800

    commit

---
+++ src/main/java/com/leekari/wechat/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.leekari.wechat.controller;
+
+import com.leekari.wechat.util.RedisUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author litao
+ * @date 2020/7/30 17:12
+ * @description
+ */
+@RestController
+public class TestController {
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @RequestMapping("/test")
+    public String test(){
+        redisUtils.set("1","3");
+        return "s";
+    }
+}
